daf64dd968439c1f6a9c2f4e4b00e784da425670
KovnerJenya <kovnerjenya@gmail.com>
2018-09-24 22:19:13 +0300

    Правки

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
 
       var popupbtn = document.querySelector(".popup-button");
       var popup = document.querySelector(".hotels-form");
+      var input1 = popup.querySelector("#arrival");
+      var input2 = popup.querySelector("#departure");
+      var submitbtn = popup.querySelector(".search-button");
+
+      popup.classList.remove("show-popup");
 
       popupbtn.addEventListener("click",function (evt) {
         evt.preventDefault();
         popup.classList.toggle("show-popup")
+        popup.classList.remove("modal-error");
       });
 
+      submitbtn.addEventListener("click", function (evt) {
+        if (!input1.value || !input2.value) {
+          evt.preventDefault();
+          popup.classList.remove("modal-error");
+          popup.offsetWidth = popup.offsetWidth;
+          popup.classList.add("modal-error");
+        }
+      });
 
 var myMap;
 
